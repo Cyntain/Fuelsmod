3bbf7f4795427bd35f794d6b59360afe11d3490b
cyntain <oliver1.harvey@gmail.com>
2013-04-14 19:26:32 +0100

    added more IDs for Items

--- fuelsmod_common/com/cyntain/Fm/lib/ItemIDs.java
+++ fuelsmod_common/com/cyntain/Fm/lib/ItemIDs.java
@@ -2,14 +2,20 @@ package com.cyntain.Fm.lib;
 
 public class ItemIDs {
 
+    
     /* Default item id */
+  
     public static int COPPER_INGOT_DEFAULT = 6483;
     public static int COPPER_CHUNK_DEFAULT = 6484;
     public static int OSMIUM_INGOT_DEFAULT = 6485;
     public static int OSMIUM_CHUNK_DEFAULT = 6486;
     public static int BERYLLIUM_INGOT_DEFAULT = 6487;
     public static int BERYLLIUM_CHUNK_DEFAULT = 6488;
+  
     public static int COPPER_CLUSTER_DEFAULT = 6489;
-   
+    public static int OSMIUM_CLUSTER_DEFAULT = 6490;
+    public static int BERYLLIUM_CLUSTER_DEFAULT = 6491;
  
+   
+   
 }
