6b521690b5884e25b811d3cf3ba4eab303ea7b4a
Paronamixxe <paronamixxe@gmail.com>
2013-04-20 14:11:06 +0200

    [Anton] Setting up the mod block file

--- fuelsmod_common/com/cyntain/Fm/Block/ModBlock.java
+++ fuelsmod_common/com/cyntain/Fm/Block/ModBlock.java
@@ -1,9 +1,22 @@
 package com.cyntain.Fm.Block;
 
+//import cpw.mods.fml.common.Mod;
+import net.minecraft.block.Block;
+import com.cyntain.Fm.lib.BlockID;
+import com.cyntain.Fm.lib.Reference;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
     public class ModBlock {
 
-        /* Mod item instances */
-    
+        /* Mod block instances */
+        public static Block copperOre ;
+        public static Block berylliumOre ;
+        public static Block osmiumOre ;
+        public static Block zeoliteOre ;
+        
+        
     
     
     
@@ -12,8 +25,12 @@ package com.cyntain.Fm.Block;
     
     public static void init() {
         
-        /* Initialise each mod item individually */
+        /* Initialise each mod block individually */
         
+            //Copper Ore
+                copperOre = new BlockCopperOre(BlockID.COPPER_ORE_DEFAULT);
+                GameRegistry.registerBlock(copperOre, Reference.MOD_ID + copperOre.getUnlocalizedName2());
+                LanguageRegistry.addName(copperOre, "Copper Ore");
         
         
   
