3dcfab18e6481e73bff1de8b24afc7b27a5dc6cc
cyntain <oliver1.harvey@gmail.com>
2013-04-20 23:18:15 +0100

    [cyntain] Added vial of zeolite

---
+++ fuelsmod_common/com/cyntain/Fm/Item/itemZeoliteVial.java
@@ -0,0 +1,76 @@
+package com.cyntain.Fm.Item;
+
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+import com.cyntain.Fm.CreativeTab.CreativeTabFm;
+import com.cyntain.Fm.lib.Reference;
+import com.cyntain.Fm.lib.Strings;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+
+public class itemZeoliteVial extends ItemFood  {
+    
+   
+    
+    private int[] potionEffect = {14};
+    private int[] potionDuration = {60};
+    private int[] potionTeir = {3};
+    
+    
+    
+    public itemZeoliteVial(int id){
+        super(id, 0, 0F, false);
+         this.setUnlocalizedName(Strings.ZEOLITE_DUST_GLASS_NAME);
+         this.setAlwaysEdible();
+         this.setCreativeTab(CreativeTabFm.tabsFuelMod);
+         this.setMaxStackSize(16);
+    }
+
+
+    @SideOnly(Side.CLIENT)
+    @Override
+            public void updateIcons(IconRegister iconRegister) {
+                    this.iconIndex = iconRegister.registerIcon(Reference.MOD_ID + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1) );
+      
+                    
+        }
+    
+
+
+public void onFoodEaten(ItemStack itemStack, World world, EntityPlayer player){
+    --itemStack.stackSize;
+    player.getFoodStats().addStats(this);
+    this.addPotionEffects(itemStack, world, player);
+}
+
+
+private void addPotionEffects(ItemStack itemStack, World world,
+        EntityPlayer player) {
+
+  player.addPotionEffect(new PotionEffect(potionEffect[0], potionDuration[0] * 20, potionTeir[0]));
+    
+}
+    public EnumAction onItemRightClick(ItemStack itemStack){
+        return EnumAction.drink;        
+    }
+
+    public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player){
+        player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
+        return itemStack;
+    }    
+        public boolean hasEffect(){
+    
+        return true;
+    }
+    
+    
+}
