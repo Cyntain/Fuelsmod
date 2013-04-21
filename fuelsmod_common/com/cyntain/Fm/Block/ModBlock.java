49d65eadbcc19a029fc287d6ca7a9d215dfb9462
cyntain <oliver1.harvey@gmail.com>
2013-04-21 17:02:12 +0100

    [cyntain] cleaned up the imports

--- fuelsmod_common/com/cyntain/Fm/Block/ModBlock.java
+++ fuelsmod_common/com/cyntain/Fm/Block/ModBlock.java
@@ -6,7 +6,6 @@ import com.cyntain.Fm.lib.Reference;
 import com.cyntain.Fm.lib.Strings;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemStack;
