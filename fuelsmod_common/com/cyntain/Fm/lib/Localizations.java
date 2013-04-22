6670795893158227d57628dda7f6641a759b5fee
Paronamixxe <paronamixxe@gmail.com>
2013-04-22 20:22:22 +0200

    [Anton] Added Pirate language file. Feel free to improve names.

--- fuelsmod_common/com/cyntain/Fm/lib/Localizations.java
+++ fuelsmod_common/com/cyntain/Fm/lib/Localizations.java
@@ -4,6 +4,6 @@ public class Localizations {
 
     private static final String LANG_RESOURCE_LOCATION = "/mods/Fm/lang/";
 
-    public static String[] localeFiles = { LANG_RESOURCE_LOCATION + "en_US.xml", LANG_RESOURCE_LOCATION + "sv_SE.xml", LANG_RESOURCE_LOCATION + "es_ES.xml", LANG_RESOURCE_LOCATION + "da_DK.xml", LANG_RESOURCE_LOCATION + "de_DE.xml", LANG_RESOURCE_LOCATION + "ja_JP.xml", LANG_RESOURCE_LOCATION + "ru_RU.xml" };
+    public static String[] localeFiles = { LANG_RESOURCE_LOCATION + "en_US.xml", LANG_RESOURCE_LOCATION + "sv_SE.xml", LANG_RESOURCE_LOCATION + "es_ES.xml", LANG_RESOURCE_LOCATION + "da_DK.xml", LANG_RESOURCE_LOCATION + "de_DE.xml", LANG_RESOURCE_LOCATION + "ja_JP.xml", LANG_RESOURCE_LOCATION + "ru_RU.xml", LANG_RESOURCE_LOCATION + "en_PT.xml" };
 
 }
