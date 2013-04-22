7fd0e6e74eecd8fe7d14545c965477ff0cc11c5b
cyntain <oliver1.harvey@gmail.com>
2013-04-22 20:16:14 +0100

    [cyntain] Added OsmiumGrate with out crafting recipe

---
+++ fuelsmod_common/com/cyntain/Fm/Item/itemOsmiumGrate.java
@@ -0,0 +1,10 @@
+package com.cyntain.Fm.Item;
+
+import com.cyntain.Fm.lib.Strings;
+
+public class itemOsmiumGrate extends ItemFm {
+ public itemOsmiumGrate(int id){
+     super(id);
+     this.setUnlocalizedName(Strings.OSMIUM_GRATE_NAME);
+ }
+}
