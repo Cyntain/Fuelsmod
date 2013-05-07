65ce71d3dbc6b2d28b23b41514359e3ae4b93d8b
Paronamixxe <paronamixxe@gmail.com>
2013-05-08 00:00:40 +0200

    [anton] messed around with trying to get the gui to show as a place holder, altough, doesnt seem to work so well...

--- fuelsmod_common/com/cyntain/Fm/Block/BlockWorktable.java
+++ fuelsmod_common/com/cyntain/Fm/Block/BlockWorktable.java
@@ -3,6 +3,8 @@ package com.cyntain.Fm.Block;
 import com.cyntain.Fm.lib.Strings;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
 public class BlockWorktable extends BlockFm {
 
@@ -13,6 +15,13 @@ public class BlockWorktable extends BlockFm {
         setHardness(5F);
         
     }
-
+    
+    public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
+    {
+            par5EntityPlayer.displayGUIWorkbench(par2, par3, par4);
+            return true;
+            //DOES NOT WORK... JUST FLASHES... NEEDS TO BE FIXED*
+    }
     
 }
+    
